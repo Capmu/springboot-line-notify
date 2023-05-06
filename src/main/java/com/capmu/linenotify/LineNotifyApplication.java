0c9cfb5dc0c45c15032d65e7c91768a0d2ef7bc7
Capmu <p.kanawat.capmu@gmail.com>
2023-05-06 21:40:14 +0700

    Initiate a Spring Boot project

---
+++ src/main/java/com/capmu/linenotify/LineNotifyApplication.java
@@ -0,0 +1,13 @@
+package com.capmu.linenotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineNotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineNotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capmu/linenotify/LineNotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capmu.linenotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineNotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
